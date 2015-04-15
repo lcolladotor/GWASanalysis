84c7a3948ba2301197b18b29df13936004af3aca
joseah <joseah@lcg.unam.mx>
2015-04-15 14:35:50 -0400

    Script to get data from relevant phenotypes

---
+++ exploratory_data_analysis/get_phenotype_data.sh
@@ -0,0 +1,9 @@
+file='selected_phenotypes-v0.1.txt'
+columns='cut -f2,6,7,8,9,10,22,27,28,31'
+
+head -n1 full | $columns
+IFS=$'\n'
+for line in $(cat $file)
+do
+    awk -v dis=$line -F $'\t' '$8 == dis' full | eval "$columns"
+done
